01e2800eac8a5d6b49e6eec410f8c6a5baf17bdc
Rupom Islam <rupomislambangladesh@gmail.com>
2026-03-16 16:53:47 +0600

    Create mar16-2026.py

---
+++ mar16-2026.py
@@ -0,0 +1,14 @@
+b = 60
+if b >= 50:
+    print("tumi r pyaso")
+elif b >= 55:
+    print("tumi a pyaso")
+else:
+    print("tumi s pyaso")
+a = 200
+if a >= 10:
+    print("tumi q ")
+elif b >= 15:
+    print("tumi w")
+else:
+    print("tumi e")
