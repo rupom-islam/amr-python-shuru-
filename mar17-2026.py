ef3ab64af80a2a3fd8276a56931c695e547db691
Rupom Islam <rupomislambangladesh@gmail.com>
2026-03-17 14:54:01 +0600

    Create mar17-2026.py

---
+++ mar17-2026.py
@@ -0,0 +1,21 @@
+a = "rupom"
+if a == "rupom":
+    print("hi rupom")
+else:
+    print(" rupom noy ")
+b = 22 
+if b >= 20:
+    print("bia giaba na")
+else:
+    print("bia giaba ")
+c = 31
+if c >= 30:
+    print("sikri kra jiaba")
+else:
+    print("sikri kra jiaba na")
+d = 20
+if d >= 20:
+    print("tumi pc yes")
+else:
+    print("tumi pc no")
+
