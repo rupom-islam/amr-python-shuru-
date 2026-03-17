e50cdd3e75528e18b7b753f3d7777301e4a90384
Rupom Islam <rupomislambangladesh@gmail.com>
2026-03-17 15:38:28 +0600

    Update mar17-2026.py

--- mar17-2026.py
+++ mar17-2026.py
@@ -19,3 +19,45 @@ if d >= 20:
 else:
     print("tumi pc no")
 
+fru = ["apple","banana","cherry"]
+for r in fru:
+    print(r)
+rrr = ["rupom","nifad","eyanur","nasim"]
+for q in rrr:
+    print(q)
+uuu = [111 ,222 ,333, 444]
+for w in uuu:
+    print(w)
+ppp = [1,2,3,4,5,]
+for z in ppp:
+    print(z)
+for r in range(8):
+    print(r)
+for u in range(4):
+    print(u)
+for p in range(7):
+    print(p)
+for q in range(11):
+    print(q)
+for w in range(101):
+    print(w)
+s = ["rr","tt","ee","aa"]
+for f in s:
+    print(f)
+for i in range(3):
+    print("Nifad")
+for j in range(100):
+    print("nifad")
+for b in range(10):
+    print("eyanur")
+f = ["aa","ss","dd","ff"]
+for l in f:
+    print(l)
+t = ["z","x","c","v"]
+for l in t:
+    print(l)
+m =["b","n","m"]
+for o in m:
+    print(o)
+
+
