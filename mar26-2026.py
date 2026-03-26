6b3d1a8216f8b876300a79140189ce5812a0cedd
Rupom Islam <rupomislambangladesh@gmail.com>
2026-03-26 11:32:29 +0600

    Create mar26-2026.py

---
+++ mar26-2026.py
@@ -0,0 +1,20 @@
+r = ["rupom","nifad","eyanur"]
+print(r)
+print(r[0])
+print(r[1])
+q = [11,22,33,44]
+print(q)
+print(q[0])
+e = "rupom"
+if e =="rupom":
+    print("tumi rupom")
+else:
+    print("tumi rupom noy")
+battery = 20
+if battery >= 17:
+    print("battery full")
+else:
+    print("battery now")
+name = "rupom"
+for e in range(10):
+    print(name)
